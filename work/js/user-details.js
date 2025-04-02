4d2414c0d0fe3fde023c7a667f937148518e68af
danyagithb <danil2019sinegub@gmail.com>
2025-04-02 17:17:12 +0200

    add some styles for page user-details

--- work/js/user-details.js
+++ work/js/user-details.js
@@ -9,10 +9,15 @@ fetch(`https://jsonplaceholder.typicode.com/users/${userId}`)
         let func = function (user) {
             for (const userKey in user) {
                 if (typeof user[userKey] === 'object') {
+                    let keyTitle = document.createElement('h3');
+                    keyTitle.classList.add('key-title')
+                    keyTitle.innerText = userKey;
+                    userDetails.appendChild(keyTitle);
+
                     func(user[userKey]);
                 } else {
                     let divBoxKey = document.createElement('div');
-                    divBoxKey.classList.add('div-box-key');
+                    divBoxKey.classList.add('box-key');
                     divBoxKey.innerText = `${userKey}: ${user[userKey]}`;
 
                     userDetails.appendChild(divBoxKey);
@@ -20,17 +25,7 @@ fetch(`https://jsonplaceholder.typicode.com/users/${userId}`)
 
             }
         }
-
         func(user);
-
-
-        // for (const userKey in user) {
-        //     let divBoxKey = document.createElement('div');
-        //     divBoxKey.classList.add('div-box-key');
-        //     divBoxKey.innerText = `${userKey}: ${user[userKey]}`;
-        //
-        //     userDetails.appendChild(divBoxKey)
-        // }
 
         button.onclick = function () {
             fetch(`https://jsonplaceholder.typicode.com/users/${userId}/posts`)
@@ -47,7 +42,7 @@ fetch(`https://jsonplaceholder.typicode.com/users/${userId}`)
 
                         let postButton = document.createElement('a');
                         postButton.classList.add('post-button');
-                        postButton.innerText = 'More about post';
+                        postButton.innerText = 'More';
                         postButton.href = `post-details.html?postId=${postItem.id}`;
                         postButton.setAttribute('target', '_blank');
 
